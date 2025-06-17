d39cb4e09e9890a5b15324bd16a452bb476ec603
herrgallardo <gallardogirela@gmail.com>
2025-06-17 09:16:38 +0200

    feat: initialize ASP.NET MVC project with Identity authentication and SQL Server

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace lxcn_movie_web_app.Data
+{
+    /// <summary>
+    /// Main database context that handles Identity authentication and will include movie data
+    /// Inherits from IdentityDbContext to provide user authentication functionality
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Configure database model relationships and constraints
+        /// </summary>
+        /// <param name="builder">Model builder for configuring entities</param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Additional model configurations will be added here
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using lxcn_movie_web_app.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Configure Identity services for authentication and authorization
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    // Configure password requirements
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add MVC services
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Authentication and Authorization middleware
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure default route
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Map Razor Pages for Identity
+app.MapRazorPages();
+
+app.Run();
